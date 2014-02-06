e608de2ae0e7cb9914ea398aa62ef817d54be709
Katherine Santiago <santiago.kat@gmail.com>
2014-02-06 01:04:59 -0500

    reverted back to prevent responsiveness

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,11 +5,17 @@ var app = app || {};
 $(document).ready(function(){
     var $container = $('#content');
 
+    while ($(window).width() <= 1133) {
+      console.log('yay');
+    }
+
     // the imagesLoaded function allows you to wait for all images to load before loading isotope so that elements don't overlap
     $container.imagesLoaded(function() {
       // loads isotope into container
       $container.isotope({
-          filter: '*',
+          // filter: '*',
+          // resizable: false,
+          // masonry: { gutterWidth: 10, columnWidth: 270, rowHeight: 360 },
           animationOptions: {
               duration: 750,
               easing: 'linear',
@@ -17,6 +23,13 @@ $(document).ready(function(){
           }
       });
     });
+
+    // $(window).smartresize(function() {
+    //   $container.isotope({
+    //     // update columnWidth to a percentage of container width
+    //     masonry: { columnWidth: $container.width() / 5 }
+    //   });
+    // });
 
     // when clicking on nav link, store data-filter attribute in 'selector'
     $('#nav a').on("click", function(e){
