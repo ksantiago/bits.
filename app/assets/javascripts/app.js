686efd5b48a6f9ed58bf3a98f2f7be9f16d99189
Katherine Santiago <santiago.kat@gmail.com>
2014-02-05 22:35:30 -0500

    fixed navigation

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,7 +5,9 @@ var app = app || {};
 $(document).ready(function(){
     var $container = $('#content');
 
+    // the imagesLoaded function allows you to wait for all images to load before loading isotope so that elements don't overlap
     $container.imagesLoaded(function() {
+      // loads isotope into container
       $container.isotope({
           filter: '*',
           animationOptions: {
@@ -14,10 +16,21 @@ $(document).ready(function(){
               queue: false,
           }
       });
+    });
 
+    $('#nav a').on("click", function(){
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      })
     });
 
     $('h1').on("click", function(){
-      console.log('yay');
+      console.log("pssst, you're cute!");
     });
 });
