edb1de77c278949ff95e2d784efe7cfc3926a6ad
Katherine Santiago <santiago.kat@gmail.com>
2014-02-05 21:19:20 -0500

    added mousover and mouseout events to each box

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,20 @@
 var app = app || {};
 
-var colors = ["#6BC36F", "#4C9BD8", "#8155AC", "#ECC133", "#66CBAC"];
+// var colors = ["#6BC36F", "#4C9BD8", "#8155AC", "#ECC133", "#66CBAC"];
 
-app.goPom = function() {
-  var randColor = colors
-  var width = $(window).width();
-  $(".hot-pink").text(width + "px").css("background-color" :);
-};
+// app.goPom = function() {
+//   var randColor = colors
+//   var width = $(window).width();
+//   $(".hot-pink").text(width + "px").css("background-color" :);
+// };
 
 $(document).ready(function() {
-  $(".container").on("click", app.goPom);
+  // $(".container").on("click", app.goPom);
+  $('.box').mouseover(function(){
+    $('.box').css("background-color", "yellow");
+  });
+
+  $('.box').mouseout(function(){
+    $('.box').css("background-color", "#FFFFFF")
+  });
 });
