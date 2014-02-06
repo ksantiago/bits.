0a97900a5d8b7bee162b02af080ed8e86c142f34
Katherine Santiago <santiago.kat@gmail.com>
2014-02-05 22:17:33 -0500

    added imagesloaded plugin to wait for images prevent overlap

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,13 +4,17 @@ var app = app || {};
 
 $(document).ready(function(){
     var $container = $('#content');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false,
-        }
+
+    $container.imagesLoaded(function() {
+      $container.isotope({
+          filter: '*',
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false,
+          }
+      });
+
     });
 
     $('h1').on("click", function(){
